644f855c5cf9411fbe99b2fb06f9a218c46eca8f
longdnk <kimlong101020@gmail.com>
2022-11-18 12:06:12 +0700

    UPDATE_17/11

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def hello_world():  # put application's code here
 
 if __name__ == '__main__':
     app.jinja_env.cache = {}
-    app.run(host='0.0.0.0', port=3000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5555, debug=True, threaded=True)
